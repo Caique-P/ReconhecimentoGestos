24830f6466911bac7605d1f59076e6a5d486f9b9
Caique Ponjjar <58194653+Caique-P@users.noreply.github.com>
2022-06-27 19:32:48 -0300

    Update recognizeLaserPointer.py

--- PythonRecognization/recognizeLaserPointer.py
+++ PythonRecognization/recognizeLaserPointer.py
@@ -1,8 +1,15 @@
 import cv2
 import numpy as np
+import socket
+
+
 cap = cv2.VideoCapture(0)
 
 pts = []
+
+ 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverAddressPort = ("127.0.0.1", 5052)
 while (1):
 
     # Take each frame
@@ -17,10 +24,19 @@ while (1):
     kernel =  np.ones((5,5),np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask)
-    print(maxLoc)
-    cv2.circle(frame, maxLoc, 20, (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.imshow('Track Laser', frame)
+    if(maxLoc[0] != 0 and maxLoc[1] != 0):
+        data = [0,0]
+        data[0] = maxLoc[0]   
+        print(maxLoc, end="\r")
+        cv2.circle(frame, maxLoc, 20, (0, 0, 255), 2, cv2.LINE_AA)
+        data[1] = maxLoc[1]
+        sock.sendto(str.encode(str(data)), serverAddressPort)
+    else:
+        print("..........", end='\r')
 
+    cv2.imshow('Track Laser', frame)
+        
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
